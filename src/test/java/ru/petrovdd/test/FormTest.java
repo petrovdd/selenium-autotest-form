fab02f7af1f0255b22c91f8a7c368e4c24108010
petrovdd <bobykot228@gmail.com>
2025-03-02 22:58:37 +0300

    Добавил тест для формы с генерацией данных, реализваол соответствующий класс для генерации, есть замечания по реализации касаемо проверки и реализации некоторых полей. Есть замечания по алгоритму основного автотеста.

--- src/test/java/ru/petrovdd/test/FormTest.java
+++ src/test/java/ru/petrovdd/test/FormTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import ru.petrovdd.page.FormPage;
-
-import java.util.concurrent.TimeUnit;
+import ru.petrovdd.util.RandomData;
 
 /**
  * Класс для проведения теста формы
@@ -33,6 +32,7 @@ class FormTest {
     @Test
     void fullRegistration() {
         driver.get(BASE_URL);
+        //TODO добавить subject
         formPage.setFirstNameField("Alex")
                 .setLastNameField("Smith")
                 .setUserEmailField("user@mail.ru")
@@ -45,6 +45,40 @@ class FormTest {
                 .setState("NCR")
                 .setCity("Delhi")
                 .submitClick()
+                .checkResult("Student Name", "Alex Smith")
+                .checkResult("Student Email", "user@mail.ru")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "7926813093")
+                .checkResult("Date of Birth", "08 November,1992")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "file.txt")
+                .checkResult("Address", "USA")
+                .checkResult("State and City", "NCR Delhi");
+        driver.quit();
+    }
+
+    /**
+     * Тест с генерацией данных
+     */
+    @Test
+    void fullRegistrationGenerateData() {
+        RandomData randomData = new RandomData();
+        driver.get(BASE_URL);
+        //TODO добавить subject
+        formPage.setFirstNameField(randomData.getFirstName())
+                .setLastNameField(randomData.getLastName())
+                .setUserEmailField(randomData.getRandomEmail())
+                .selectGenderWrapperField(randomData.getRandomGender())
+                .setUserNumberField(randomData.getPhoneNumber())
+                .setDateOfBirthInput(randomData.getDay(), randomData.getMonthName(), randomData.getYear())
+                .selectHobbies(randomData.getRandomHobbies())
+                //TODO подумать над генерацией пути для файла
+                .setUploadFile("C:\\selenium-autotest\\src\\test\\resources\\file.txt")
+                .setCurrentAddress(randomData.getFullAddress())
+                .setState(randomData.getRandomState())
+                .setCity(randomData.getRandomCity())
+                .submitClick()
+                //TODO изменить все проверки
                 .checkResult("Student Name", "Alex Smith")
                 .checkResult("Student Email", "user@mail.ru")
                 .checkResult("Gender", "Male")
---
+++ src/test/java/ru/petrovdd/util/RandomData.java
@@ -0,0 +1,207 @@
+package ru.petrovdd.util;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+import java.util.Random;
+
+/**
+ * Класс для генерации данных
+ */
+public class RandomData {
+
+    private final Random random = new Random();
+    private final String randomGender;
+    private final String randomHobbies;
+    private final String randomState;
+    private final String randomCity;
+    private final String randomFirstName;
+    private final String randomLastName;
+    private final String randomEmail;
+    private final String randomPhoneNumber;
+    private final String randomFullAddress;
+    private final String randomMonthName;
+    private final String randomYear;
+    private final String randomDay;
+    //TODO реализовать поле для subject
+
+    /**
+     * Конструктор класса, генерируем данные и инициализируем поля
+     */
+    public RandomData() {
+        Faker faker = new Faker(new Locale("en-GB"));
+        randomGender = generateRandomGender();
+        randomHobbies = generateRandomHobbies();
+        randomState = generateRandomState();
+        randomCity = generateRandomCity();
+        randomMonthName = generateMonthName();
+        randomYear = String.valueOf(getRandomInt(1970, 2025));
+        //TODO для дня генерация костыльная, подумать над разной верхней границей в зависимости от месяца
+        randomDay = String.valueOf(getRandomInt(1, 28));
+        randomFirstName = faker.name().firstName();
+        randomLastName = faker.name().lastName();
+        randomEmail = faker.internet().emailAddress();
+        randomPhoneNumber = faker.phoneNumber().phoneNumber();
+        randomFullAddress = faker.address().fullAddress();
+
+    }
+
+    /**
+     * Получить пол
+     */
+    public String getRandomGender() {
+        return randomGender;
+    }
+
+    /**
+     * Получить хобби
+     */
+    public String getRandomHobbies() {
+        return randomHobbies;
+    }
+
+    /**
+     * Получить email
+     */
+    public String getRandomEmail() {
+        return randomEmail;
+    }
+
+    /**
+     * Получить страну
+     */
+    public String getRandomState() {
+        return randomState;
+    }
+
+    /**
+     * Получить город
+     */
+    public String getRandomCity() {
+        return randomCity;
+    }
+
+    /**
+     * Получить имя
+     */
+    public String getFirstName() {
+        return randomFirstName;
+    }
+
+    /**
+     * Получить фамилию
+     */
+    public String getLastName() {
+        return randomLastName;
+    }
+
+    /**
+     * Получить номер телефона
+     */
+    public String getPhoneNumber() {
+        return randomPhoneNumber;
+    }
+
+    /**
+     * Получить полный адрес проживания
+     */
+    public String getFullAddress() {
+        return randomFullAddress;
+    }
+
+    /**
+     * Получить наименование месяца
+     */
+    public String getMonthName() {
+        return randomMonthName;
+    }
+
+    /**
+     * Получить год
+     */
+    public String getYear() {
+        return randomYear;
+    }
+
+    /**
+     * Получить день
+     */
+    public String getDay() {
+        return randomDay;
+    }
+
+    /**
+     * Сгенерировать пол
+     *
+     * @return пол
+     */
+    private String generateRandomGender() {
+        String[] genders = {"Male", "Female", "Other"};
+        return getRandomItemFromArray(genders);
+    }
+
+    /**
+     * Сгенерировать хобби
+     *
+     * @return наименование хобби
+     */
+    private String generateRandomHobbies() {
+        String[] hobbies = {"Sports", "Reading", "Music"};
+        //TODO хобби может быть несколько, подумать над выбором нескольких
+        return getRandomItemFromArray(hobbies);
+    }
+
+    /**
+     * Сгенерировать страну
+     *
+     * @return наименование страны
+     */
+    private String generateRandomState() {
+        String[] states = {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
+        return getRandomItemFromArray(states);
+    }
+
+    /**
+     * Сгенерировать город
+     *
+     * @return наименование города
+     */
+    private String generateRandomCity() {
+        String[] city = {"Delhi", "Gurgaon", "Noida"};
+        return getRandomItemFromArray(city);
+    }
+
+    /**
+     * Сгенерировать наименование месяца
+     *
+     * @return наименование месяца
+     */
+    private String generateMonthName() {
+        //TODO попробовать вынести все это в список/списки
+        String[] genders = {"January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"};
+        return getRandomItemFromArray(genders);
+    }
+
+    /**
+     * Получить рандомный элемент массива
+     *
+     * @param array массив данных
+     * @return рандомный элемент массива
+     */
+    private String getRandomItemFromArray(String[] array) {
+        return array[random.nextInt(array.length)];
+    }
+
+    /**
+     * Получить случайное сгенерированное число
+     *
+     * @param min минимальный порог числа
+     * @param max максимальный порог числа
+     * @return случайное сгенерированное число
+     */
+    private int getRandomInt(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+}

6112d898a3395adfdda0558c0fc2ed10afd37165
petrovdd <bobykot228@gmail.com>
2025-03-14 23:24:31 +0300

    Починил отчетность, тест, не добавил теги

--- src/test/java/ru/petrovdd/properties/test/SystemPropertiesTest.java
+++ src/test/java/ru/petrovdd/properties/test/SystemPropertiesTest.java
@@ -3,12 +3,12 @@ package ru.petrovdd.properties.test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class SystemPropertiesTests {
+class SystemPropertiesTest {
 
     @Test
     @DisplayName("Параметризированный тест")
-    void systemPropertiesTest() {
-        String browser = System.getProperty("browser");
+    public void systemPropertiesTest() {
+        String browser = System.getProperty("BROWSER");
         System.out.println("Browser: " + browser);
     }
 
